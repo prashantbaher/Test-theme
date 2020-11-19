73e083240297a10c17cdd0e7b9f6e07283171219
SungracePrashant <prashant@sungrace-group.com>
2020-11-19 14:22:45 +0530

    baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'The Cad Coder',
   tagline: 'Free SOLIDWORKS API Tutorials for Engineers.',
   url: 'https://prashantbaher.github.io/',
-  baseUrl: '/',
+  baseUrl: '/Test-theme/',
   onBrokenLinks: 'throw',
   favicon: 'img/logo.png',
   organizationName: 'prashantbaher', // Usually your GitHub org/user name.
